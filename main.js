992f5a428f7a40f69bae7f733afc6a970aed4f16
Fredrik Eklund <fredrik@bayville.se>
2024-02-13 10:03:55 +0100

    Javascript och CSS

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+
+
 async function fetchPokemon(id) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const data = await response.json();
+
+    
     console.log(data);
     return data;
 }
 
 
-fetchPokemon(1);
+
+for (let i = 1; i <= 10; i++) {
+    fetchPokemon(i)
+}
