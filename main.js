cf404f092c510ed9585bc733371e9bfc010e9918
Fredrik Eklund <fredrik@bayville.se>
2024-02-13 10:24:02 +0100

    Javascript anropt och utskrift till DOM

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-
-
 async function fetchPokemon(id) {
+    //Gör fetchanrop
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const data = await response.json();
-
     
-    console.log(data);
-    return data;
+    //Lagra svar i variabel
+    const data = await response.json();                                         
+    
+    //Container för pokemons
+    const pokemonContainerEl = document.getElementById('pokemonContainer');     
+    
+    //Skriv ut pokemon till container
+    pokemonContainerEl.innerHTML += `                                          
+        <div class="pokemon__card">
+            <img src="${data.sprites.front_default}" alt="${data.name}">
+            <h2>${data.name}</h2>
+        </div>`;
 }
 
-
-
-for (let i = 1; i <= 10; i++) {
+//Loop som anropar funktionen fetchPokemon med ett id för den aktuella loopen
+for (let i = 1; i <= 12; i++) {
     fetchPokemon(i)
 }
